38524d89825fc0bbe7b20f1406ce8243e7d55fd3
edudanntas <eduardodanttas@icloud.com>
2023-01-19 22:33:29 -0300

    updated javascript

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 const form = document.querySelector('form');
 const nlwSetup = new NLWSetup(form);
+const button = document.querySelector('header button');
 
-const data = {
-    gym: ['01-19', '01-20', '01-21', '01-22', '01-23', '01-24', '01-25', '01-26', '01-27', '01-28']
+button.addEventListener('click', add)
+form.addEventListener('change', save)
+
+function add() {
+    const today = new Date().toLocaleDateString('pt-br').slice(0, -5);
+    const dayExists = nlwSetup.dayExists(today);
+
+    if (dayExists) {
+        alert('Dia já incluso! :c');
+        return
+    };
+    nlwSetup.addDay(today);
+    alert('Dia adicionado com sucesso! c:');
+};
+
+function save() {
+    localStorage.setItem('HABIT@setupEdu', JSON.stringify(nlwSetup.data));
 }
 
+const data = JSON.parse(localStorage.getItem('HABIT@setupEdu')) || {};
 nlwSetup.setData(data);
 nlwSetup.load();
+
